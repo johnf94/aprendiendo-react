51b0bf5110ce75ac5e607d2984d74fa89458ee09
Jonathan Franco <jdfp.master@gmail.com>
2023-08-26 22:11:28 -0600

    add standard lint

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,30 +1,26 @@
 import { useState } from 'react'
-import confetti from  'canvas-confetti'
+import confetti from 'canvas-confetti'
 
 import './App.css'
-import { Square }  from './components/Square.jsx'
+import { Square } from './components/Square.jsx'
 import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
 
-
-function App() {
-  const [board, setBoard] = useState( ()=>{
+function App () {
+  const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
-    return boardFromStorage ?
-      JSON.parse(boardFromStorage) :  Array(9).fill(null)
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
   })
-  const [turn, setTurn] = useState( ()=>
-  {
+  const [turn, setTurn] = useState(() => {
     const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.X
-
   }
   )
-  const [winner, setWinner] = useState(null) 
+  const [winner, setWinner] = useState(null)
   // null no hay ganador, false empate
-
-  
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
@@ -34,57 +30,55 @@ function App() {
     window.localStorage.removeItem('turn')
   }
 
-  
-  const updateBoard = (index)=>{
-    //no actualizamos esta posicion si ya tiene algo
-    if(board[index] || winner)return
-    //actualizamos el tablero
-    const newBoard = [... board]
+  const updateBoard = (index) => {
+    // no actualizamos esta posicion si ya tiene algo
+    if (board[index] || winner) return
+    // actualizamos el tablero
+    const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    //cambiar el turno
+    // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
-    //guardar aquí partida 
-    //se guarda el tablero y el turno para saber quien le tocaba
-    //el board se guarda en formato Json porque la funcion guarda en modo string
+    // guardar aquí partida
+    // se guarda el tablero y el turno para saber quien le tocaba
+    // el board se guarda en formato Json porque la funcion guarda en modo string
     window.localStorage.setItem('board', JSON.stringify(newBoard))
     window.localStorage.setItem('turn', newTurn)
-    //revisar si hay ganador
+    // revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti()
-      setWinner(newWinner)      
+      setWinner(newWinner)
     }// ToDo: check si el juego ya terminó
-     else if(checkEndGame(newBoard)){
-      setWinner(false)//empate
+    else if (checkEndGame(newBoard)) {
+      setWinner(false)// empate
     }
-
   }
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reiniciar el Juego</button>
       <section className='game'>
-      { 
+        {
         board.map((_, index) => {
-          return(
+          return (
             <Square
               key={index}
               index={index}
               updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
+            >
+              {board[index]}
+            </Square>
           )
         })
       }
       </section>
       <section className='turn'>
-        <Square isSelected = {turn === TURNS.X}>
+        <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
         </Square>
-        <Square isSelected = {turn === TURNS.O}>
+        <Square isSelected={turn === TURNS.O}>
           {TURNS.O}
         </Square>
       </section>
--- projects/02-tic-tac-toe/src/logic/board.js
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -1,27 +1,27 @@
-import { WINNER_COMBOS } from "../constants";
+import { WINNER_COMBOS } from '../constants'
 
 export const checkWinnerFrom = (boardToCheck) => {
-    //revisamos todas las combinaciones ganadoras
-    //para ver si X o 0 ganó
-    for(const combo of WINNER_COMBOS){
-      const [a,b,c] = combo
-      if( 
-        boardToCheck[a] && // 0 -> x o 0
+  // revisamos todas las combinaciones ganadoras
+  // para ver si X o 0 ganó
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] && // 0 -> x o 0
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
+    ) {
+      return boardToCheck[a]
     }
-    //si no hay ganador 
-    return null;
   }
+  // si no hay ganador
+  return null
+}
 
-  export const checkEndGame= (newBoard)=>{
-    //revisamos si hay un empate
-    //si no hay mas espacios vacios
-    //en el tablero
+export const checkEndGame = (newBoard) => {
+  // revisamos si hay un empate
+  // si no hay mas espacios vacios
+  // en el tablero
 
-    //newBoard = [x,0,x,0,x,0,x,0]
-    return newBoard.every((square)=> square !== null) // x o 0
-  }
+  // newBoard = [x,0,x,0,x,0,x,0]
+  return newBoard.every((square) => square !== null) // x o 0
+}
